22d1a0c41f22cdf6e209d620886eb36c2df22a7a
Nuno Miranda <nuno_miranda@outlook.com>
2024-05-17 18:25:59 +0100

    update

--- App.tsx
+++ App.tsx
@@ -17,6 +17,7 @@ export default function App() {
       backgroundColor="transparent"
       translucent
       />
+      
       { fontsLoaded? <Groups/> :  <Loading />}
     </ThemeProvider>
   );
